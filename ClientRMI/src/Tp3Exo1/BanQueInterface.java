625e97e17dcf47369e2ffc0fe4f9c1486d2124d8
zerrouki djamel <djameljimmizerrouki@gmail.com>
2020-08-22 20:35:34 +0200

    1234

--- ClientRMI/src/Tp3Exo1/BanQueInterface.java
+++ ClientRMI/src/Tp3Exo1/BanQueInterface.java
@@ -5,6 +5,7 @@ import java.rmi.RemoteException;
 import java.util.Date;
 public interface BanQueInterface extends Remote
 {
+//MyApp 
   public  Date date () throws RemoteException;
   public  double convert (double d1) throws RemoteException;
   
