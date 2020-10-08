b72af279a8bf683585e343215e86c81a3bdb66e0
zerrouki djamel <djameljimmizerrouki@gmail.com>
2020-10-08 17:09:35 +0200

    date

--- ClientRMI/src/Tp3Exo2/MathInterface.java
+++ ClientRMI/src/Tp3Exo2/MathInterface.java
@@ -4,9 +4,13 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 public interface MathInterface extends Remote
 {
+// add
   public  double add (double d1, double d2) throws RemoteException;
+// sub
   public  double sub (double d1, double d2) throws RemoteException;
+// mul
   public  double Mul (double d1, double d2) throws RemoteException;
+// div
   public  double Div (double d1, double d2) throws RemoteException;
 
 } 
